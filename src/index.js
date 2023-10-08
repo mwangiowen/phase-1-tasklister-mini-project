6fb28b9fb3a871437978392b7f9d2ed4246ac3e9
mwangiowennjugunasmith <owen.njugun@student.moringa.com>
2023-10-09 01:50:15 +0300

    completed asignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("create-task-form");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();})
   // your code here
+});
+form.addEventListener("submit", function (event) {
+  event.preventDefault(); 
+  
+  const taskDescription = document.getElementById("new-task-description").value;
+
 });
